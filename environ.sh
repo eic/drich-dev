3a06a930a60180b66f6d34d671927a6bbfe91845
christopher dilks <christopher.j.dilks@gmail.com>
2021-10-21 01:07:06 -0400

    update env

--- environ.sh
+++ environ.sh
@@ -2,6 +2,9 @@
 # note: juggler and athena share the same install prefix ...bad idea? We shall see...
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ATHENA_PREFIX/lib
 export JUGGLER_DETECTOR=athena
+export BEAMLINE_CONFIG=ip6
+export BEAMLINE_CONFIG_VERSION=master
+export JUGGLER_DETECTOR_VERSION=master
 export DETECTOR_PATH=$(pwd)/athena
 export DETECTOR_VERSION=master
 export JUGGLER_INSTALL_PREFIX=$ATHENA_PREFIX
@@ -10,3 +13,4 @@ export JUGGLER_INSTALL_PREFIX=$ATHENA_PREFIX
 export JUGGLER_SIM_FILE=$(pwd)/sim/sim_run.root
 export JUGGLER_REC_FILE=test.root
 export JUGGLER_N_EVENTS=100
+export JUGGLER_RNG_SEED=1
--- runJuggler.sh
+++ runJuggler.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # run juggler
+
+option=JugPID/options/testIRT.py
+#option=JugPID/options/rich_reco.py
+
 source environ.sh
 pushd juggler
-xenv -x $JUGGLER_INSTALL_PREFIX/Juggler.xenv \
-  gaudirun.py JugPID/options/testIRT.py
+xenv -x $JUGGLER_INSTALL_PREFIX/Juggler.xenv gaudirun.py $option
 popd
