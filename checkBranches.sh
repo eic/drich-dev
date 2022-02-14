f4324e67e8e17a3dec07b367a970e7685e48958b
christopher dilks <christopher.j.dilks@gmail.com>
2022-02-14 13:03:24 -0500

    modified:   checkBranches.sh

--- checkBranches.sh
+++ checkBranches.sh
@@ -3,7 +3,6 @@
 wd=$(pwd)
 for repo in irt ip6 athena eicd juggler reconstruction_benchmarks; do
   cd $repo
-  echo $repo: $(git branch | awk '/^\*/{print $2}')
+  printf "%30s: %s\n" $repo $(git branch | awk '/^\*/{print $2}')
   cd $wd
-done | column -t
-  
+done
