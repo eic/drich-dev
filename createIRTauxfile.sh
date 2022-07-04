8a32e523570b207af769f6b594b0d837eec7221f
christopher dilks <christopher.j.dilks@gmail.com>
2022-07-04 18:08:51 -0400

    mv createIRTauxfile.py from `ecce/scripts` to here

--- createIRTauxfile.sh
+++ createIRTauxfile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# create IRT geometry config file
+# - create IRT geometry config file
+# - wraps scripts/createIRTauxfile.py, filtering verbose `stdout` for IRT-specific printouts
 outFile=geo/irt-drich.root
-python ecce/scripts/create_IRT_auxfile.py -o $outFile | grep IRTLOG
+scripts/createIRTauxfile.py -o $outFile | grep IRTLOG
 echo ""
 echo "produced $outFile"
 echo ""
