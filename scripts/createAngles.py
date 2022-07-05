de9fbd49c952ae01d48ef8f785dbcaeddf2a4277
Jesse-Campbell <108422158+Jesse-Campbell@users.noreply.github.com>
2022-07-05 15:35:24 -0400

    Update createAngles.py

--- scripts/createAngles.py
+++ scripts/createAngles.py
@@ -4,24 +4,24 @@
 
 import numpy as np
 import sys
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 circle = 2 * np.pi
+epsilon = 1e-2  # to avoid zero division
 
 
-def makeAngles(theta_min: float, theta_max: float, theta_increment: float, hit_density: int) -> list:
+def makeAngles(theta_min: float, theta_max: float, num_rings: int, hit_density: int) -> list:
     """
-    :param theta_min: minimum polar angle of photon, in radian4
-    :param theta_max: maximum polar angle of photon, in radians
-    :param theta_increment: increment of polar angle between theta_min and theta_max
-    :param hit_density: the amount of photon hits for the smallest polar angle
+    :param theta_min: minimum polar angle of dRICH in radians
+    :param theta_max: maximum polar angle of dRICH in radians
+    :param num_rings: number of concentric rings
+    :param hit_density: the amount of photon hits for the minimum polar angle
     :return: returns a list of tuples containing polar and azimuthal angles, in radians, in the form
     [(polar, azimuthal), (...), ...] for generating photons that will have an even distribution on the dRICH sensor
     """
 
     angles = []
-    rings = int(np.round((theta_max - theta_min)/theta_increment))
-    thetas = np.linspace(theta_min, theta_max, num=rings)
-    phis = np.linspace(hit_density, hit_density * (theta_increment + 2), num=rings, dtype=int)
+    thetas = np.linspace(theta_min, theta_max, num=num_rings)
+    phis = np.linspace(hit_density, hit_density * (theta_max / (theta_min + epsilon)), num=num_rings, dtype=int)
 
     for r, t in convert(thetas, phis):
         angles.append(tuple((r, t)))
@@ -33,16 +33,16 @@ def makeAngles(theta_min: float, theta_max: float, theta_increment: float, hit_d
 
 
 def convert(thetas, phis):
-    # Helper function for makeAngles()
+    # Helper function for makePositions()
     for i in range(len(thetas)):
         for j in range(phis[i]):
-            yield thetas[i], j * (circle/phis[i])
+            yield thetas[i], j * (circle / phis[i])
 
 
-if __name__ == '__main__':  # Code for running makeAngles() in command line
+if __name__ == '__main__':  # Code for running makePositions() in command line
     args = sys.argv
     theta_min = float(args[2])
     theta_max = float(args[3])
-    theta_increment = float(args[4])
+    num_rings = int(args[4])
     hit_density = int(args[5])
-    globals()[args[1]](theta_min, theta_max, theta_increment, hit_density)
+    globals()[args[1]](theta_min, theta_max, num_rings, hit_density)
