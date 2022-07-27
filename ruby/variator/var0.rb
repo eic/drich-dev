4875602be50fd852d6e53ca3c75f7949a5eeccd0
christopher dilks <christopher.j.dilks@gmail.com>
2022-07-27 02:26:47 -0400

    update optics test pipeline

--- ruby/variator/var0.rb
+++ ruby/variator/var0.rb
@@ -1,62 +1,28 @@
-require './ruby/variator/functions.rb'
+require './ruby/variator/variator_base.rb'
 
-class Variator < VariationFunctions
+class Variator < VariatorBase
   def initialize
 
-    ### PARAMETER VARIATIONS *************************
     @varied_settings = [
       {
         :xpath     => '//mirror',
         :attribute => 'focus_tune_x',
         :function  => @@center_delta,
         :args      => [70, 20],
+        :count     => 4,
+      },
+      {
+        :xpath     => '//mirror',
+        :attribute => 'focus_tune_z',
+        :function  => @@min_max,
+        :args      => [20, 50],
         :count     => 3,
       },
-      # {
-      #   :xpath     => '//mirror',
-      #   :attribute => 'focus_tune_z',
-      #   :function  => @@min_max,
-      #   :args      => [20, 50],
-      #   :count     => 3,
-      # },
     ]
 
-    ### FIXED SETTINGS *******************************
-    @fixed_settings = [
-      { :constant=>'DRICH_debug_optics', :value=>'1' },
-    ]
+    @fixed_settings = []
 
-    ### SIMULATION COMMANDS **************************
-    @simulation_pipelines = Proc.new do |settings|
-      [
-        # [[
-        #   "./simulate.py",
-        #   "-t 1",
-        #   "-n 100",
-        #   "-c #{settings[:compact]}",
-        #   "-o #{settings[:output]}",
-        # ]],
-        # [[
-        #   "./drawHits.exe",
-        #   settings[:output],
-        # ]],
-        [
-          ## visualize parallel-to-point focal region
-          ## NOTE: use Xvfb to suppress OGL windows (`xvfb-run opt/eic-shell`)
-          ["exit"],
-          [
-            "./simulate.py",
-            "-t12",
-            "-v",
-            "-msvg",
-            "-c#{settings[:compact]}",
-            "-o#{settings[:output]}",
-          ],
-        ],
-      ]
-    end
-
-    ### **********************************************
+    @simulation_pipelines = @@test_optics
 
   end
   attr_accessor :varied_settings, :fixed_settings, :simulation_pipelines
---
+++ ruby/variator/variator_base.rb
@@ -0,0 +1,61 @@
+require 'numpy'
+
+# Variator base class, containing common code for all variators
+class VariatorBase
+
+  ### VARIATION FUNCTIONS **************************
+  # - you are welcome to add any variation functions here
+  # - python methods are available via the 'pycall' gem (such as 'numpy')
+  # - must return Array (use `.to_a` to convert a numpy array to a ruby Array)
+
+  # linearly vary by `center +/- delta`, `count` times
+  @@center_delta = Proc.new do |center, delta, count|
+    Numpy.linspace(center-delta, center+delta, count).to_a
+  end
+
+  # linearly vary from `min` to `max`, `count` times
+  @@min_max = Proc.new do |min, max, count|
+    Numpy.linspace(min, max, count).to_a
+  end
+
+
+  ### SIMULATION COMMANDS **************************
+  # - common pipeline lists, for example optics testing
+  # - you are welcome to add your own here too
+  # - see template.rb for syntax details
+
+  ### run some tests for optics optimization
+  @@test_optics = Proc.new do |settings|
+    [
+      ### be sure optics-debugging mode is disabled
+      [[ 'scripts/edit_xml.rb', settings[:compact_drich], '//constant[@name="DRICH_debug_optics"]', 'value', '0' ]],
+      ### run simulation to check hit rings at varying theta
+      [[
+        './simulate.py',
+        '-t 4',
+        '-n 100',
+        "-c #{settings[:compact_detector]}",
+        "-o #{settings[:output]}",
+      ]],
+      ### draw the hits
+      [[ './drawHits.exe', settings[:output] ]],
+      ### enable optics-debugging mode: all components become vacuum, except for mirrors
+      [[ 'scripts/edit_xml.rb', settings[:compact_drich], '//constant[@name="DRICH_debug_optics"]', 'value', '1' ]],
+      ### visualize parallel-to-point focal region
+      #   - NOTE: use Xvfb to suppress OGL windows (`xvfb-run opt/eic-shell`)
+      #   - `exit` is piped to automatically exit the interactive G4 shell
+      [
+        ["exit"],
+        [
+          "./simulate.py",
+          "-t 12",
+          "-v",
+          "-m svg",
+          "-c #{settings[:compact_detector]}",
+          "-o #{settings[:output]}",
+        ],
+      ],
+    ]
+  end
+
+end
