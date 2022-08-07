f152034cf29cfff96731c5a915f63fa44c419bff
christopher dilks <christopher.j.dilks@gmail.com>
2022-08-07 13:17:54 -0400

    new file:   ruby/variator/optics1.rb

---
+++ ruby/variator/optics1.rb
@@ -0,0 +1,48 @@
+# optics tuning
+require './ruby/variator/variator_base.rb'
+
+class Variator < VariatorBase
+  def initialize
+    super
+
+    @varied_settings = [
+      {
+        :xpath     => '//mirror',
+        :attribute => 'focus_tune_x',
+        :function  => @@center_delta,
+        :args      => [10, 20],
+        :count     => 5,
+      },
+      {
+        :xpath     => '//mirror',
+        :attribute => 'focus_tune_z',
+        :function  => @@center_delta,
+        :args      => [30, 30],
+        :count     => 5,
+      },
+      {
+        :xpath     => '//sensors//sphere',
+        :attribute => 'radius',
+        :function  => @@min_max,
+        :args      => [80, 160],
+        :count     => 5,
+        :label     => :sensor_sphere_radius,
+      },
+    ]
+
+    @derived_settings = [
+      {
+        :xpath      => '//sensors//sphere',
+        :attribute  => 'centerz',
+        :derivation => Proc.new{ |value| 50.0 - value[:sensor_sphere_radius] },
+      },
+    ]
+
+    @simulation_pipelines = @@test_optics
+
+    # best variants:
+    #
+    # TODO
+
+  end
+end
