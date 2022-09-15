73212e087bc3a771159bd965b932813c4a10e91b
Christopher Dilks <c-dilks@users.noreply.github.com>
2022-09-14 20:16:49 -0400

    Add momentum scan plots (#39)

--- scripts/momentum_scan.rb
+++ scripts/momentum_scan.rb
@@ -7,27 +7,37 @@ require 'fileutils'
 require 'pycall/import'
 
 ## settings
-TestNum   = 7                   # test number for simulate.py
-NumEvents = 300                 # number of events per fixed momentum
-PoolSize  = 4                   # number of parallel threads to run
-OutputDir = 'out/momentum_scan' # output directory ( ! will be overwritten ! )
-DrawOnly  = false               # if true, do not run simulation, only draw the result
-MaxCounts = 320                 # vertical axis limit of the counts vs. momentum plot
+NumEvents      = 100                 # number of events per fixed momentum
+NumPoints      = 20                  # number of momenta to sample
+PoolSize       = 6                   # number of parallel threads to run
+OutputDir      = 'out/momentum_scan' # output directory ( ! will be overwritten ! )
+RunSimRec      = true                # if false, do not run simulation+reconstruction, only draw the result
+UseRINDEXrange = false               # if true, use range of RINDEX values rather than a single reference value
 
 ## list of particles to test
-particle_a = [
-  'e-',
-  'pi+',
-  'kaon+',
-  'proton',
-  # 'e+',
-  # 'pi-',
-  # 'kaon-',
-  # 'anti_proton',
-]
+particle_h = {
+  'e-'          => { :mass=>0.00051, },
+  'pi+'         => { :mass=>0.13957, },
+  'kaon+'       => { :mass=>0.49368, },
+  'proton'      => { :mass=>0.93827, },
+  # 'e+'          => { :mass=>0.00051, },
+  # 'pi-'         => { :mass=>0.13957, },
+  # 'kaon-'       => { :mass=>0.49368, },
+  # 'anti_proton' => { :mass=>0.93827, },
+}
+
+## radiators
+## FIXME: :rIndexRef values are averages? 
+radiator_h = {
+  :agl => { :id=>0, :testNum=>7, :rIndexRef=>1.0190, :rIndexRange=>[1.01852,1.02381], },
+  :gas => { :id=>1, :testNum=>8, :rIndexRef=>1.0008, :rIndexRange=>[1.00075,1.00084], },
+}
+
+## warn if not doing simulation
+puts "\nNOTE: skipping simulation+reconstruction, since RunSimRec=false\n\n" unless RunSimRec
 
 ## produce output file dir and names
-unless DrawOnly
+if RunSimRec
   FileUtils.rm_r OutputDir, secure: true, verbose: true, force: true
   FileUtils.mkdir_p OutputDir
 end
@@ -35,45 +45,67 @@ def out_file(prefix,ext)
   "#{OutputDir}/#{prefix}.#{ext}"
 end
 
-## run momentum scan simulation for each particle in `particle_a`
-unless DrawOnly
-  particle_a.each_slice(PoolSize) do |slice|
-    pool = slice.map do |particle|
-      # simulation command
-      cmds = []
+## run momentum scan simulation for each particle in `particle_h`
+particle_h.keys.product(radiator_h.keys).each_slice(PoolSize) do |slice|
+  pool = slice.map do |particle,rad_name|
+    rad = radiator_h[rad_name]
+    # preparation
+    sim_file = out_file particle, "sim.#{rad_name}.root"
+    rec_file = out_file particle, "rec.#{rad_name}.root"
+    cmds = []
+    # simulation + reconstruction
+    if RunSimRec
       cmds << [
         './simulate.py',
-        "-t#{TestNum}",
+        "-t#{rad[:testNum]}",
         '-s',
         "-p#{particle}",
         "-n#{NumEvents}",
-        "-o#{out_file particle, 'root'}",
+        "-k#{NumPoints}",
+        "-o#{sim_file}",
       ]
       cmds << [
-        'bin/draw_hits',
-        out_file(particle,'root'),
+        './recon.sh',
+        "-j",
+        "-i #{sim_file}",
+        "-o #{rec_file}",
       ]
-      # spawn thread
-      Thread.new do
-        cmds.each_with_index do |cmd,i|
-          puts cmd.join ' '
-          mode = i==0 ? 'w' : 'a'
-          Open3.pipeline(
-            cmd,
-            out: [out_file(particle,'log.out'),mode],
-            err: [out_file(particle,'log.err'),mode],
-          )
-        end
+      # analysis
+      plot_file = out_file particle, "rec_plots.#{rad_name}.root"
+      cmds << [
+        'root', '-b', '-q',
+        "scripts/src/momentum_scan_draw.C(\"#{rec_file}\",\"#{plot_file}\",#{rad[:id]})"
+      ]
+    end
+    # spawn thread
+    Thread.new do
+      cmds.each_with_index do |cmd,i|
+        puts cmd.join ' '
+        mode = i==0 ? 'w' : 'a'
+        Open3.pipeline(
+          cmd,
+          out: [out_file(particle,'log.out'),mode],
+          err: [out_file(particle,'log.err'),mode],
+        )
       end
     end
-    trap 'INT' do
-      pool.each &:kill
-      exit 1
-    end
-    # wait for pool to finish
-    pool.each &:join
   end
+  trap 'INT' do
+    pool.each &:kill
+    exit 1
+  end
+  # wait for pool to finish
+  pool.each &:join
 end
+
+# print errors for one of the particles
+puts "ERRORS (for one particle) ======================="
+system "cat #{out_file particle_h.keys.first, 'log.err'}"
+puts "END ERRORS ======================================"
+
+
+#############################################################
+
 
 ## start ROOT analysis
 # - must be done after simulations, otherwise this script hangs
@@ -82,52 +114,126 @@ r.gROOT.SetBatch true
 r.gStyle.SetOptStat 0
 r.gStyle.SetLegendTextSize 0.1
 
-## draw settings
-default_color = r.kBlack
-default_marker = r.kFullCircle
-particle_h = particle_a.map do |particle|
-  [
-    particle,
-    {
-      :root_file => r.TFile.new(out_file(particle,'plots.root')),
-      :color     => default_color,
-      :marker    => default_marker,
-    }.to_h
-  ]
-end.to_h
-particle_h['e-'][:color]      = r.kBlack
-particle_h['pi+'][:color]     = r.kBlue
-particle_h['kaon+'][:color]   = r.kGreen+1
-particle_h['proton'][:color]  = r.kMagenta
-particle_h['e-'][:marker]     = r.kFullCircle
-particle_h['pi+'][:marker]    = r.kFullSquare
-particle_h['kaon+'][:marker]  = r.kFullTriangleUp
-particle_h['proton'][:marker] = r.kFullTriangleDown
+## loop over radiators
+radiator_h.each do |rad_name,rad|
 
-## draw
-canv_hits = r.TCanvas.new 'canv_hits', 'canv_hits', 1000, 800
-leg_hits = r.TLegend.new 0, 0.7, 1, 1
-canv_hits.Divide 2,1
-pad_plot = r.TPad.new 'pad_plot', 'pad_plot', 0,    0, 0.75, 1
-pad_leg  = r.TPad.new 'pad_leg',  'pad_leg',  0.75, 0, 1,    1
-pad_plot.SetGrid 1,1
-pad_plot.Draw
-pad_leg.Draw
-particle_h.each do |particle,h|
-  pad_plot.cd
-  plot = h[:root_file].Get 'aveHitsVsP'
-  plot.SetMarkerStyle h[:marker]
-  plot.SetMarkerColor h[:color]
-  plot.SetLineColor   h[:color]
-  plot.SetMarkerSize  1.5
-  plot.GetYaxis.SetRangeUser 0, MaxCounts
-  plot.Draw 'SAME E X0'
-  plot.GetXaxis.SetTitle 'Thrown momentum [GeV]'
-  plot.GetYaxis.SetTitle 'Average number of hits'
-  plot.SetTitle 'Average ' + plot.GetTitle
-  leg_hits.AddEntry plot, particle, 'PE'
-end
-pad_leg.cd
-leg_hits.Draw
-canv_hits.SaveAs out_file('_ave_hits_vs_p','png')
-particle_h.values.each{ |h| h[:root_file].Close }
+  ## draw settings
+  default_color  = r.kBlack
+  default_marker = r.kFullCircle
+  draw_h = particle_h.keys.map do |particle|
+    [
+      particle,
+      {
+        :root_file => r.TFile.new(out_file(particle, "rec_plots.#{rad_name}.root")),
+        :color     => default_color,
+        :marker    => default_marker,
+      }.to_h
+    ]
+  end.to_h
+  draw_h['e-'][:color]      = r.kBlack
+  draw_h['pi+'][:color]     = r.kBlue
+  draw_h['kaon+'][:color]   = r.kGreen+1
+  draw_h['proton'][:color]  = r.kMagenta
+  draw_h['e-'][:marker]     = r.kFullCircle
+  draw_h['pi+'][:marker]    = r.kFullSquare
+  draw_h['kaon+'][:marker]  = r.kFullTriangleUp
+  draw_h['proton'][:marker] = r.kFullTriangleDown
+
+  ## loop over plots (loop through the first particle's file, assume the rest have the same)
+  first_root_file = draw_h[particle_h.keys.first][:root_file]
+  PyCall.iterable(first_root_file.GetListOfKeys).each do |tkey|
+    plot_name = tkey.GetName
+    next unless plot_name.match? /scan_pfx$/
+    
+    ## canvas
+    canv = r.TCanvas.new "#{plot_name}_#{rad_name}_canv", "#{plot_name}_#{rad_name}_canv", 1000, 800
+    leg_hits = r.TLegend.new 0, 0.7, 1, 1
+    canv.Divide 2,1
+    pad_plot = r.TPad.new 'pad_plot', 'pad_plot', 0,    0, 0.75, 1
+    pad_leg  = r.TPad.new 'pad_leg',  'pad_leg',  0.75, 0, 1,    1
+    pad_plot.SetGrid 1,1
+    pad_plot.SetLeftMargin 0.2
+    pad_plot.Draw
+    pad_leg.Draw
+
+    ## loop over particles
+    rad[:maxTheta] = 0
+    draw_h.each do |particle,h|
+
+      ## get the plot
+      plot = h[:root_file].Get plot_name
+
+      ## variables for this particle and radiator
+      mass   = particle_h[particle][:mass]
+      rindex = rad[:rIndexRef]
+
+      ## minimum momentum for Cherenkov emission
+      def calculate_mom_min(m,n)
+        m / Math.sqrt(n**2-1)
+      end
+      mom_min = calculate_mom_min mass, rindex
+
+      ## drop points with momentum < mom_min
+      (1..plot.GetNbinsX).each do |bn|
+        mom = plot.GetBinCenter bn
+        if mom < mom_min
+          plot.SetBinContent bn, 0
+          plot.SetBinError   bn, 0
+          plot.SetBinEntries bn, 0
+        end
+      end
+
+      ## saturation Cherenkov angle
+      def calculate_max_theta(n)
+        1000 * Math.acos(1/n)
+      end
+      max_theta = calculate_max_theta rindex
+      rad[:maxTheta] = [rad[:maxTheta],max_theta].max
+
+      ## add plot to canvas
+      pad_plot.cd
+      plot.SetMarkerStyle h[:marker]
+      plot.SetMarkerColor h[:color]
+      plot.SetLineColor   h[:color]
+      plot.SetMarkerSize  1.5
+      plot.Draw 'SAME E X0'
+      plot.GetYaxis.SetTitle 'Average ' + plot.GetTitle.sub(/ vs\..*/,'')
+      plot.SetTitle          'Average ' + plot.GetTitle
+      leg_hits.AddEntry plot, particle, 'PE'
+
+      ## expected Cherenkov angle curve
+      if plot_name.match?(/^theta_/)
+        rindex_list = UseRINDEXrange ? rad[:rIndexRange] : [rindex]
+        rindex_list.each_with_index do |n,i| 
+          ftn_theta = r.TF1.new(
+            "ftn_theta_#{particle}_#{rad_name}_#{i}",
+            "1000*TMath::ACos(TMath::Sqrt(x^2+#{mass}^2)/(#{n}*x))",
+            calculate_mom_min(mass,n),
+            plot.GetXaxis.GetXmax,
+          )
+          ftn_theta.SetLineColor h[:color]
+          ftn_theta.SetLineWidth 2
+          ftn_theta.Draw 'SAME'
+          # set plot maximum
+          rad[:maxTheta] = [rad[:maxTheta],calculate_max_theta(n)].max # max expected
+          rad[:maxTheta] = [rad[:maxTheta],plot.GetMaximum].max        # max point
+        end
+      end
+      plot.GetYaxis.SetRangeUser(0,1.1*rad[:maxTheta]) if plot_name.match?(/^theta_/)
+
+    end
+
+    ## save canvas to file
+    pad_leg.cd
+    leg_hits.Draw
+    if plot_name.match?(/^nphot_/) # :gas and :agl plots are the same, just save :gas
+      canv.SaveAs out_file("_#{plot_name}",'png') if rad_name==:gas 
+    else
+      canv.SaveAs out_file("_#{plot_name}.#{rad_name}",'png')
+    end
+
+  end # loop over plots
+
+  draw_h.values.each{ |h| h[:root_file].Close }
+
+end # loop over radiators
---
+++ scripts/src/momentum_scan_draw.C
@@ -0,0 +1,166 @@
+// draw various momentum scan plots
+R__LOAD_LIBRARY(podioDict)
+R__LOAD_LIBRARY(podioRootIO)
+R__LOAD_LIBRARY(edm4hep)
+R__LOAD_LIBRARY(edm4eic)
+R__LOAD_LIBRARY(fmt)
+
+#include "podio/EventStore.h"
+#include "podio/ROOTReader.h"
+#include "edm4hep/utils/kinematics.h"
+#include "fmt/format.h"
+
+// radiators
+enum rad_enum {kAgl=0, kGas=1};
+
+//=========================================================
+
+void momentum_scan_draw(
+    std::string rec_file_name  = "out/rec.root",
+    std::string out_file_name  = "out/rec.scan_plots.root",
+    unsigned    which_radiator = 0 // see `rad_enum` above
+    )
+{
+  // open event store
+  podio::ROOTReader reader;
+  podio::EventStore store;
+  reader.openFile(rec_file_name);
+  store.setReader(&reader);
+
+  // output root file
+  TFile *out_file = new TFile(TString(out_file_name),"RECREATE");
+
+  // radiators
+  std::string radiator_name;
+  double mom_max;
+  switch(which_radiator) {
+    case kAgl:
+      radiator_name = "Aerogel";
+      mom_max       = 22;
+      break;
+    case kGas:
+      radiator_name = "Gas";
+      mom_max       = 65;
+      break;
+    default:
+      fmt::print(stderr,"ERROR: unknown which_radiator={}\n",which_radiator);
+      return;
+  }
+
+  // plot limits
+  const double theta_max = 260.0;
+  const int    nphot_max = 500;
+  const int    npe_max   = 0.3*nphot_max;
+
+  // define histograms
+  std::map<std::string,TH2D*> scans;
+  auto make_scan = [&scans,&mom_max,&radiator_name] (
+      std::string name,
+      std::string title,
+      std::string units,
+      auto yn, auto yi, auto yf
+      )
+  {
+    if(units!="") units=" ["+units+"]";
+    std::string for_radiator = (name!="nphot") ? ", for "+radiator_name : "";
+    auto hist = new TH2D(
+        TString(name + "_scan"),
+        TString(title + " vs. Thrown Momentum" + for_radiator +";p [GeV];" + title + units),
+        (int)mom_max+1, 0, mom_max,
+        yn, yi, yf
+        );
+    hist->StatOverflows(true);
+    scans.insert({name,hist});
+  };
+  make_scan( "nphot", "N_{photons}",        "",     nphot_max, 0, nphot_max );
+  make_scan( "npe",   "N_{photoelectrons}", "",     npe_max,   0, npe_max   );
+  make_scan( "theta", "#theta_{Cherenkov}", "mrad", 100,       0, theta_max );
+
+  // function to Fill a scan: warns for under/overflows
+  auto FillScan = [&scans] (std::string name, auto x, auto y) {
+    auto check = [] (auto val, std::string ax_name, TAxis *ax) {
+      if(val<ax->GetXmin() || val>ax->GetXmax())
+        fmt::print(stderr,"WARNING: {} overflow: {}\n",ax_name,val);
+    };
+    auto scan = scans.at(name);
+    check(x, name+" scan x-axis", scan->GetXaxis());
+    check(y, name+" scan y-axis", scan->GetYaxis());
+    scan->Fill(x,y);
+  };
+
+  // event loop =================================================================
+  for(unsigned e=0; e<reader.getEntries(); e++) {
+
+    // get next event
+    // fmt::print("read event #{}\n",e);
+    if(e>0) {
+      store.clear();
+      reader.endOfEvent();
+    };
+
+    // get collections
+    const auto& mcParts = store.get<edm4hep::MCParticleCollection>("MCParticles");
+    const auto& hits    = store.get<edm4hep::SimTrackerHitCollection>("DRICHHits");
+    const auto& cpids   = store.get<edm4eic::CherenkovParticleIDCollection>("DRICHPID");
+
+    // thrown momentum
+    int nthrown = 0;
+    float mom;
+    for(const auto& mcPart : mcParts) {
+      if(mcPart.getGeneratorStatus()==1) {
+        mom = edm4hep::utils::p(mcPart);
+        nthrown++;
+      }
+    }
+    if(nthrown == 0) { fmt::print(stderr,"WARNING: no thrown particle found for event #{}\n",e); continue; };
+    if(nthrown >  1)   fmt::print(stderr,"WARNING: this script does not yet support multi-track events (nthrown={})\n",nthrown);
+
+    // number of photons
+    FillScan("nphot", mom, hits.size());
+
+    // NPE, Cherenkov angle
+    if(cpids.size() != 1) fmt::print(stderr,"WARNING: CherenkovParticleIDCollection size = {} != 1\n",cpids.size());
+    for(const auto& cpid : cpids) {
+      for(const auto &meas : cpid.getAngles()) {
+        auto npe = meas.npe;
+        auto theta = meas.theta * 1000; // [mrad]
+        bool pass =
+          meas.radiator == which_radiator
+          && npe>0
+          ;
+        if(pass) {
+          FillScan( "npe",   mom, npe   );
+          FillScan( "theta", mom, theta );
+        }
+      }
+    }
+
+  } // end event loop =================================================================
+  store.clear();
+  reader.endOfEvent();
+  reader.closeFile();
+
+  // make scan profiles
+  std::map<std::string,TProfile*> scan_profs;
+  std::map<std::string,TCanvas*> scan_canvs;
+  for(const auto& [name,scan] : scans) {
+    auto prof = scan->ProfileX("_pfx",1,-1,"");
+    prof->SetLineColor(kBlack);
+    prof->SetLineWidth(3);
+    scan_profs.insert({name,prof});
+    TString canv_name(name+"_canv");
+    auto canv = new TCanvas(canv_name,canv_name);
+    canv->SetGrid(1,1);
+    scan->Draw("box");
+    prof->Draw("same");
+    scan_canvs.insert({name,canv});
+  }
+
+  // write output
+  auto WriteScans = [] (auto vec) { for(const auto& [name,obj] : vec) obj->Write(); };
+  WriteScans(scans);
+  WriteScans(scan_profs);
+  WriteScans(scan_canvs);
+  out_file->Close();
+  fmt::print("\n{:=<50}\nwrote: {}\n\n","",out_file_name);
+}
