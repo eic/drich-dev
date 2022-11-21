019e6fed5ae3e54d752e52e1dfe491c7fa4e15e1
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-11-21 10:10:35 -0500

    modified:   check_status.sh

--- check_branches.sh
+++ check_branches.sh
@@ -2,7 +2,7 @@
 # check which branch you are on for each repository
 wd=$(pwd)
 echo ""
-for repo in . epic EDM4eic irt juggler reconstruction_benchmarks; do
+for repo in . epic EDM4eic irt EICrecon juggler reconstruction_benchmarks; do
   if [ -d "$repo" ]; then
     cd $repo
     printf "%30s: %s  (%s)\n" $(basename `pwd`) $(git branch | awk '/^\*/{print $2}') $(git rev-parse --short HEAD)
