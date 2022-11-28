c8ab74581720a057519cfc0056340de94423fdaa
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-11-27 23:53:30 -0500

    modified:   rebuild_all.sh

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -2,7 +2,7 @@
 # rebuild all modules, in order of dependence
 # pass an argument to rebuild everything cleanly
 set -e
-./build.sh EDM4eic $*
-./build.sh irt     $*
-./build.sh epic    $*
-./build.sh juggler $*
+./build.sh EDM4eic  $*
+./build.sh irt      $*
+./build.sh epic     $*
+./build.sh EICrecon $*
