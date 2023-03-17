bfd3259de201af45a48ec05d9917956be596ca5d
Christopher Dilks <christopher.j.dilks@gmail.com>
2023-03-17 16:57:44 -0400

    new file:   test-run.sh

---
+++ test-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source prefix2/install/bin/eicrecon-this.sh
+echo $ROOT_INCLUDE_PATH
+echo "EXIT PREMATURELY"; exit
+
+# export JANA_PLUGIN_PATH=prefix2/install/lib/EICrecon/plugins${JANA_PLUGIN_PATH:+:${JANA_PLUGIN_PATH}}
+# echo "JANA_PLUGIN_PATH = $JANA_PLUGIN_PATH"
+
+# export ROOT_INCLUDE_PATH=prefix2/install/include/IRT${ROOT_INCLUDE_PATH:+:${ROOT_INCLUDE_PATH}}
+# echo "ROOT_INCLUDE_PATH = $ROOT_INCLUDE_PATH"
+
+prefix2/install/bin/eicrecon \
+  -Ppodio:output_include_collections=DRICHHits \
+  -Ppodio:output_file=out/rec.root \
+  -Pjana:nevents=0 \
+  -Pjana:debug_plugin_loading=1 \
+  -Pacts:MaterialMap=calibrations/materials-map.cbor \
+  out/sim.root
