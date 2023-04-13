8efe31c60fc2c787e59af715301ac201e2e964c8
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-04-13 13:42:30 -0400

    revert:   rebuild_all.sh

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -3,7 +3,7 @@
 # pass an argument to rebuild everything cleanly
 set -e
 ./build.sh EDM4eic  $*
-# ./build.sh irt      $*  # FIXME: force us to test with container build
+./build.sh irt      $*
 ./build.sh epic     $*
 source environ.sh
 ./build.sh EICrecon $*
