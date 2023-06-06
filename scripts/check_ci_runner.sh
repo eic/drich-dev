8c709adc8ac9ab7b4daee66b023f5e5acfde1c1d
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-06-06 19:58:42 -0400

    fix: set `x` permission for executables

--- scripts/check_ci_runner.sh
+++ scripts/check_ci_runner.sh
@@ -6,3 +6,6 @@ echo "[CI] PREFIX TREE"
 [ -d "prefix" ] && find prefix || echo "no prefix tree"
 echo "[CI] BRANCHES"
 check_branches.sh
+echo "[CI] UPDATE PERMISSIONS"
+chmod 755 bin -Rv
+[ -d "prefix" ] && chmod 755 prefix/bin -Rv
