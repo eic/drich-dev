d45caf10656b7a72c0b092ddf6cc6dccfea42e4b
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-10-05 12:22:31 -0400

    test: `unique_ptr` in DD4hep_service

--- scripts/configure_CI.sh
+++ scripts/configure_CI.sh
@@ -17,7 +17,7 @@ for repo in $repo_list; do
       git clone https://github.com/eic/irt.git --branch main
       ;;
     EICrecon)
-      git clone https://github.com/eic/EICrecon.git --branch main
+      git clone https://github.com/eic/EICrecon.git --branch shared-ptr-const-detector
       ;;
     reconstruction_benchmarks)
       git clone https://eicweb.phy.anl.gov/EIC/benchmarks/reconstruction_benchmarks.git --branch master
